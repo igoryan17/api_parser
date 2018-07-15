d08b196a5a764c947750679ada77acb936b4f10d
Igor Yadrov <igor.yadrov@netcracker.com>
2018-07-15 16:44:16 +0300

    parse links to api

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import urllib.request
 import ssl
+import links_parser
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 page = urllib.request.urlopen('https://10.101.72.106/docs/rest/', context=ctx)
-print(page)
+links_parser.parse_links_to_api(page)
--- links_parser.py
+++ links_parser.py
@@ -1,2 +1,12 @@
-import urllib.request
+from lxml import etree
+
+
+def parse_links_to_api(page):
+    parser = etree.HTMLParser()
+    tree = etree.fromstring(page.read(), parser)
+    links = tree.xpath("//a[not(contains(@href, '#'))]/@href")
+    result = []
+    for link in links:
+        result.append(page.url + link)
+    return result
 
