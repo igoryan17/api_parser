07155561ca2aee2d5fd8d0ebb2bd626320fc0f1c
igya0716 <igor.yadrov@netcracker.com>
2018-07-16 17:03:13 +0300

    small fix

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import json
-import urllib.request
 import ssl
-
 import time
+import urllib.request
+
 from selenium import webdriver
 
 import links_parser
@@ -23,9 +23,17 @@ for link in links_parser.parse_links_to_api(page):
     collection = postman_collection_writer.create_main_body(tittle)
     for section in links_parser.parse_sections(driver):
         article = links_parser.parse_article_name(section)
+        if not article:
+            continue
         method = links_parser.parse_query_type(article)
+        if not method:
+            continue
         body = links_parser.parse_request_body(section)
+        if body.lower() == 'na':
+            body = ''
         api_url = links_parser.parse_request_url(section)
+        if not api_url:
+            continue
         item = postman_collection_writer.create_item(article, method, body, api_url)
         collection['item'].append(item)
     with open('out/' + tittle.replace(' ', '_') + '.json', 'w') as outfile:
