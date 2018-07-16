246b825a4d50b8a96bb6a2583661dea3d91d419c
igya0716 <igor.yadrov@netcracker.com>
2018-07-16 16:44:17 +0300

    finish

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import urllib.request
 import ssl
 
@@ -13,20 +14,21 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 page = urllib.request.urlopen('https://10.101.72.106/docs/rest/', context=ctx)
-links = links_parser.parse_links_to_api(page)
-link = next(iter(links))
-
 driver = webdriver.Chrome('C:\Tools\chromedriver.exe')
-driver.get(link)
-time.sleep(LOADING_WAIT_TIME)
-tittle = links_parser.parse_page_tittle(driver)
-collection = postman_collection_writer.create_main_body(tittle)
-for section in links_parser.parse_sections(driver):
-    article = links_parser.parse_article_name(section)
-    method = links_parser.parse_query_type(article)
-    body = links_parser.parse_request_body(section)
-    api_url = links_parser.parse_request_url(section)
-    item = postman_collection_writer.create_item(article, method, body, api_url)
-    collection['item'].append(item)
+
+for link in links_parser.parse_links_to_api(page):
+    driver.get(link)
+    time.sleep(LOADING_WAIT_TIME)
+    tittle = links_parser.parse_page_tittle(driver)
+    collection = postman_collection_writer.create_main_body(tittle)
+    for section in links_parser.parse_sections(driver):
+        article = links_parser.parse_article_name(section)
+        method = links_parser.parse_query_type(article)
+        body = links_parser.parse_request_body(section)
+        api_url = links_parser.parse_request_url(section)
+        item = postman_collection_writer.create_item(article, method, body, api_url)
+        collection['item'].append(item)
+    with open('out/' + tittle.replace(' ', '_') + '.json', 'w') as outfile:
+        json.dump(collection, outfile)
+
 driver.close()
-print(collection)
